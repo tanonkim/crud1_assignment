32b93d7ae0cf6258b93474563dad4983c3149fcf
tanonkim <robbins4bos@gmail.com>
2022-02-12 21:07:29 +0900

    Adding get & read

--- products/views.py
+++ products/views.py
@@ -7,6 +7,7 @@ from products.models import Menu, Category, Drink
 
 
 class ProductsView(View):
+    # Create
     def post(self, request):
         data = json.loads(request.body)
         menu = Menu.objects.create(name=data["menu"])
@@ -14,4 +15,20 @@ class ProductsView(View):
         Drink.objects.create(name=data["product"], category=category, menu=menu)
 
         return JsonResponse({"messasge": "created"}, status=201)
+
+    # Read
+    def get(self, request):
+        products = Drink.objects.all()
+        results = []
+
+        for product in products:
+            results.append(
+                {
+                    "menu": product.category.menu.name,
+                    "category": product.category.name,
+                    "product_name": product.name,
+                }
+            )
+
+        return JsonResponse({"results": results}, status=200)
 
